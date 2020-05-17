5f189fbab75b35bd7674155fb8813ab3ab504630
GerasimovDmitry <gerasimovdm17@gmail.com>
2020-05-18 01:24:21 +0400

    JWT added

--- src/main/java/com/quest/backend/repository/UserRepository.java
+++ src/main/java/com/quest/backend/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findAllByLogin(String login);
+    User getUserByLogin(String login);
     User findAllByEmail(String email);
 }
