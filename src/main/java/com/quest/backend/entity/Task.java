ff4600e8a5dc746f42fa038e983cd132f174cb6c
GerasimovDmitry <gerasimovdm17@gmail.com>
2020-05-24 14:36:44 +0400

    Basic Task added

--- src/main/java/com/quest/backend/entity/Task.java
+++ src/main/java/com/quest/backend/entity/Task.java
@@ -1,4 +1,40 @@
 package com.quest.backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "task")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid")
+    @Column(name = "UUID")
+    @Size(max = 32)
+    private String uuid;
+
+    @Column(name = "answer", nullable = false)
+    private String answer;
+
+    @ManyToOne
+    @JoinColumn(name="section_uuid")
+    private String sectionUUID;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "serial_number")
+    private int serialNumber;
+
+    @Column(name = "test")
+    private String text;
+
+    @Column(name = "url")
+    private String url;
 }
--- src/main/java/com/quest/backend/service/TaskRepositoryService.java
+++ src/main/java/com/quest/backend/service/TaskRepositoryService.java
@@ -1,4 +1,23 @@
 package com.quest.backend.service;
 
+import com.quest.backend.entity.Task;
+import com.quest.backend.repository.TaskRepository;
+import com.quest.backend.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
 public class TaskRepositoryService {
+
+    private final TaskRepository repository;
+    @Autowired
+    public TaskRepositoryService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+    public Task getAllBysectionUUID(String sectionUUID) {
+        return repository.getAllBysectionUUID(sectionUUID);
+    }
 }
