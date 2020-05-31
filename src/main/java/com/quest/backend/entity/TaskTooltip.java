d2757ba92c900969c5c5aae4b2115c50908cf2b1
GerasimovDmitry <gerasimovdm17@gmail.com>
2020-05-31 15:35:54 +0400

    entity taskTooltip added

--- src/main/java/com/quest/backend/entity/TaskTooltip.java
+++ src/main/java/com/quest/backend/entity/TaskTooltip.java
@@ -1,4 +1,21 @@
 package com.quest.backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task_tooltip")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskTooltip {
+    @Column(name = "task_uuid", nullable = false)
+    private String taskUUID;
+    @Column(name = "tooltip_uuid", nullable = false)
+    private String tooltipUUID;
 }
