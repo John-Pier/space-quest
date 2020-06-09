b414e4c9044731c79b286bc818f1733e7d0578ed
GerasimovDmitry <gerasimovdm17@gmail.com>
2020-06-09 17:33:35 +0400

    userProfile was added on be

--- src/main/java/com/quest/backend/controller/UserController.java
+++ src/main/java/com/quest/backend/controller/UserController.java
@@ -2,26 +2,29 @@ package com.quest.backend.controller;
 
 import com.quest.backend.config.Constants;
 import com.quest.backend.entity.User;
+import com.quest.backend.entity.models.UserProfile;
 import com.quest.backend.service.UserRepositoryService;
 import com.quest.backend.token.JwtTokenUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @Slf4j
+@CrossOrigin
 @RequestMapping(Constants.API_VERSION)
-@CrossOrigin(origins = "*", maxAge = 4800, methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT}, allowedHeaders = "*")
 public class UserController {
-    @Autowired
     private UserRepositoryService userRepositoryService;
 
+    public UserController(UserRepositoryService userRepositoryService) {
+        this.userRepositoryService = userRepositoryService;
+    }
+
     @GetMapping("/user")
-    public User getUserByLogin(@RequestHeader("Authorization") String token) {
+    public UserProfile getUserByLogin(@RequestHeader("Authorization") String token) {
         JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
         String login = jwtTokenUtil.getUsernameFromToken(token.substring(7));
         log.info("Get user: "+ login);
-        return userRepositoryService.getUserByLogin(login);
+        return userRepositoryService.getUserProfileByLogin(login);
     }
 }
